27b0d21b9d8214fc21f09a8e10c179228d62115f
Mohammad Shahzeb <mohammadshahzeb0012@gmail.com>
2024-08-19 15:24:28 +0530

    seciond commit

--- service/globalApies.js
+++ service/globalApies.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const API_KEY = import.meta.env.VITE_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-    baseURL: 'https://ai-resume-builder-backend-9wbm.onrender.com/',
+    baseURL: 'https://ai-resume-builder-backend-9wbm.onrender.com',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${API_KEY}`
